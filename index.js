62fa14b1a9781f9f5432a1abfa96b2684eae7120
Evgeniy <foksmix256@gmail.com>
2022-12-10 16:15:00 +0200

    fixed problems

--- index.js
+++ index.js
@@ -16,21 +16,22 @@ const io = socket(server, {
     cors: {
         origin: "https://socket-frontend-test.vercel.app",
     },
+    transports: ["websocket", "polling"],
 })
 
 io.on("connect_error", (err) => {
-    console.log(`connect_error due to ${err.message}`);
-  });
+    console.log(`connect_error due to ${err.message}`)
+})
 
 io.on("connection", (socket) => {
     console.log("user connected " + socket.id)
 
-    socket.on('join_room', (data) => {
+    socket.on("join_room", (data) => {
         socket.join(data)
     })
 
     socket.on("send_message", (data) => {
-        socket.to(data.room).emit('receive_message', {message: data.message})
+        socket.to(data.room).emit("receive_message", { message: data.message })
         // broadcast
     })
 })
